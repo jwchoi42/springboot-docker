48277decd1f734f6737e0f374843370f6f9039ec
최지웅 <jwchoi42@naver.com>
2024-02-25 23:22:53 +0900

    init:

---
+++ src/main/java/com/ssafy/backend/HealthCheckController.java
@@ -0,0 +1,37 @@
+package com.ssafy.backend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+@RestController
+public class HealthCheckController {
+
+    @Value("${server.env}")
+    private String env;
+    @Value("${server.port}")
+    private String serverPort;
+    @Value("${server.serverAddress}")
+    private String serverAddress;
+    @Value("${server.serverName}")
+    private String serverName;
+
+    @GetMapping("/health-check")
+    public ResponseEntity<?> healthCheck() {
+        Map<String, String> responseData = new TreeMap<>();
+        responseData.put("serverName", serverName);
+        responseData.put("serverAddress", serverAddress);
+        responseData.put("serverPort", serverPort);
+        responseData.put("env", env);
+        return ResponseEntity.ok(responseData);
+    }
+
+    @GetMapping("/env")
+    public ResponseEntity<?> getEnv() {
+        return ResponseEntity.ok(env);
+    }
+}
